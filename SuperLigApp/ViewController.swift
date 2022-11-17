8c54a12e4003f13be55a121c06b86b7e773889a0
hadanur <115171695+hadanur@users.noreply.github.com>
2022-11-17 16:38:47 +0300

    Initial Commit

---
+++ SuperLigApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperLigApp
+//
+//  Created by Hakan Adanur on 17.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
